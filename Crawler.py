7e98e2fb1c917b2577a49baee6ccca20b58d18aa
krokiet <200762@student.pwr.edu.pl>
2015-11-09 20:47:17 +0100

    random page crawler

--- Crawler.py
+++ Crawler.py
@@ -1,22 +1,25 @@
 import threading
 import logging
 import pycurl
-from os.path import devnull
+import random
 
 
 class Crawler(threading.Thread):
-
     def __init__(self):
         super(Crawler, self).__init__()
         self.c = pycurl.Curl()
+        self.responseTimes = 0
+        self.verbose = False
         self.login_string = "krokiet"
         self.password_string = "pkpk11"
-        self.responseTimes = 0
+        self.pages = ['home', 'accountManagement', 'flat', 'mailbox', 'about', 'aboutFlat', 'aboutMoney',
+                      'aboutChores']
 
     def run(self):
         self.init()
         self.login()
-        self.page("flat")
+        # self.page("flat")
+        self.crawl(20)
         self.logout()
         return
 
@@ -28,7 +31,7 @@ class Crawler(threading.Thread):
     def login(self):
         logging.debug('trying to login')
         self.c.setopt(self.c.URL, 'http://piotrkomar.pl/HelloWorld/login')
-        self.c.setopt(self.c.POSTFIELDS, 'login='+self.login_string+'&password='+self.password_string)
+        self.c.setopt(self.c.POSTFIELDS, 'login=' + self.login_string + '&password=' + self.password_string)
         self.c.setopt(pycurl.WRITEFUNCTION, lambda x: None)
         self.c.perform()
         self.handle_response()
@@ -41,15 +44,22 @@ class Crawler(threading.Thread):
         self.handle_response()
         return
 
+    def crawl(self, jumps):
+        for i in range(1, jumps):
+            self.page(random.choice(self.pages))
+
+        return
+
     def page(self, page):
-        logging.debug('trying to acces '+page)
-        self.c.setopt(self.c.URL, 'http://piotrkomar.pl/HelloWorld/'+page)
+        logging.debug('trying to acces ' + page)
+        self.c.setopt(self.c.URL, 'http://piotrkomar.pl/HelloWorld/' + page)
         self.c.perform()
         self.handle_response()
         return
 
     def handle_response(self):
         self.responseTimes += self.c.getinfo(self.c.TOTAL_TIME)
-        print('Status: %d' % self.c.getinfo(self.c.RESPONSE_CODE))
-        print('Response time: %f' % self.c.getinfo(self.c.TOTAL_TIME))
+        if self.verbose:
+            print('Status: %d' % self.c.getinfo(self.c.RESPONSE_CODE))
+            print('Response time: %f' % self.c.getinfo(self.c.TOTAL_TIME))
         return
--- tester.py
+++ tester.py
@@ -2,8 +2,11 @@
 import logging
 import Crawler
 
-logging.basicConfig(level=logging.DEBUG, format='(%(threadName)-10s) %(message)s',)
+# level=CRITICAL, ERROR, WARNING,INFO, DEBUG, NOTSET
+logging.basicConfig(level=logging.ERROR, format='(%(threadName)-10s) %(message)s',)
 
 for i in range(20):
     thread = Crawler.Crawler()
     thread.start()
+    thread.join()
+    print('Total response time: %f' % thread.total_response)
