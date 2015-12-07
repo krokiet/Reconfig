be57208d0e3c152d2b420402d8760c75925967b4
Jakub Gargul <jakub.gargul@gmail.com>
2015-12-07 20:03:07 +0100

    almost good tests

--- tester.py
+++ tester.py
@@ -5,6 +5,8 @@ import os
 import Crawler
 import timed_user
 import sync_utils
+import statistics
+import sys
 
 # level=CRITICAL, ERROR, WARNING,INFO, DEBUG, NOTSET
 logging.basicConfig(level=logging.ERROR, format='(%(threadName)-10s) %(message)s',)
@@ -15,23 +17,21 @@ def clear_cookies():
     for f in files:
         os.remove(f)
 
+print("\n\n\n users per second \n\n\n")
 
 # timed_users
 def test1():
-    for num_of_users_per_sec in [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 200]:
-        number_of_runs = 5
-        total_mean_response_time = 0
-        total_longest_response_time = ('', 0)
-        total_lowest_response_time = ('', 123456789)
+    for num_of_users_per_sec in [20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300]:
+        number_of_runs = 1
 
+        all_threads = []
         for run in range(number_of_runs):
             clear_cookies()
             thread_count = num_of_users_per_sec
             cut_length = int(0.1 * thread_count)
-            all_threads = []
 
             for i in range(thread_count):
-                thread = timed_user.timed_user(i, 1/num_of_users_per_sec * i)
+                thread = timed_user.timed_user(i, 1.0/num_of_users_per_sec * i)
                 thread.start()
                 all_threads.append(thread)
 
@@ -44,40 +44,37 @@ def test1():
             for thread in all_threads:
                 thread.join()
 
-            list_of_tuples = [item for sublist in [item.responses for item in all_threads] for item in sublist]
-            if cut_length != 0:
-                list_of_tuples = list_of_tuples[cut_length:-cut_length]
-
-            longest_response_time = max(list_of_tuples, key=lambda item: item[1])
-            if longest_response_time[1] > total_longest_response_time[1]:
-                total_longest_response_time = longest_response_time
-            lowest_response_time = min(list_of_tuples, key=lambda item: item[1])
-            if lowest_response_time[1] < total_lowest_response_time[1]:
-                total_lowest_response_time = lowest_response_time
-            mean_response_time = float(sum(y for x, y in list_of_tuples)) / max(len(list_of_tuples), 1)
-            total_mean_response_time += mean_response_time
-
             sync_utils.running = False
 
-        total_mean_response_time /= number_of_runs
-        print('Longest response time: {0}s, Lowest response time: {1}s Mean response time: {2}s with {3} instant log-in, log-out users introduced per second'
-              .format(total_longest_response_time, total_lowest_response_time, total_mean_response_time, num_of_users_per_sec))
+        list_of_tuples = [item for sublist in [item.responses for item in all_threads] for item in sublist]
+        if cut_length != 0:
+            list_of_tuples = list_of_tuples[cut_length:-cut_length]
+
+        longest_time = max(list_of_tuples, key=lambda item: item[1])
+        lowest_time = min(list_of_tuples, key=lambda item: item[1])
+        mean_time = statistics.mean([item[1] for item in list_of_tuples])
+        median_time = statistics.median([item[1] for item in list_of_tuples])
+        std_deviation = statistics.stdev([item[1] for item in list_of_tuples])
+        length = len(list_of_tuples)
+        error_count = length - [item[0] for item in list_of_tuples].count('HTTP200')
+
+        print('longest time: {0} lowest time: {1} mean time: {2} median time: {3} deviation: {4} numOf responses: {5} error count: {6} with {7} instant log-in, log-out users inserted per second'
+              .format(longest_time, lowest_time, mean_time, median_time, std_deviation, length, error_count, num_of_users_per_sec))
+        sys.stdout.flush()
 test1()
 
+print("\n\n\n crawl x20 \n\n\n")
 
 # parallel crawling
 def test2():
-    for num_of_users in [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150]:
-        number_of_runs = 5
-        total_mean_response_time = 0
-        total_longest_response_time = ('', 0)
-        total_lowest_response_time = ('', 123456789)
+    for num_of_users in [20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300]:
+        number_of_runs = 1
 
+        all_threads = []
         for run in range(number_of_runs):
             clear_cookies()
             thread_count = num_of_users
             cut_length = int(0.1 * thread_count)
-            all_threads = []
 
             for i in range(thread_count):
                 thread = Crawler.Crawler(i, 20)
@@ -93,40 +90,37 @@ def test2():
             for thread in all_threads:
                 thread.join()
 
-            list_of_tuples = [item for sublist in [item.responses for item in all_threads] for item in sublist]
-            if cut_length != 0:
-                list_of_tuples = list_of_tuples[cut_length:-cut_length]
-
-            longest_response_time = max(list_of_tuples, key=lambda item: item[1])
-            if longest_response_time[1] > total_longest_response_time[1]:
-                total_longest_response_time = longest_response_time
-            lowest_response_time = min(list_of_tuples, key=lambda item: item[1])
-            if lowest_response_time[1] < total_lowest_response_time[1]:
-                total_lowest_response_time = lowest_response_time
-            mean_response_time = float(sum(y for x, y in list_of_tuples)) / max(len(list_of_tuples), 1)
-            total_mean_response_time += mean_response_time
-
             sync_utils.running = False
 
-        total_mean_response_time /= number_of_runs
-        print('Longest response time: {0}s, Lowest response time: {1}s Mean response time: {2}s with {3} crawling users'
-              .format(total_longest_response_time, total_lowest_response_time, total_mean_response_time, num_of_users))
+        list_of_tuples = [item for sublist in [item.responses for item in all_threads] for item in sublist]
+        if cut_length != 0:
+            list_of_tuples = list_of_tuples[cut_length:-cut_length]
+
+        longest_time = max(list_of_tuples, key=lambda item: item[1])
+        lowest_time = min(list_of_tuples, key=lambda item: item[1])
+        mean_time = statistics.mean([item[1] for item in list_of_tuples])
+        median_time = statistics.median([item[1] for item in list_of_tuples])
+        std_deviation = statistics.stdev([item[1] for item in list_of_tuples])
+        length = len(list_of_tuples)
+        error_count = length - [item[0] for item in list_of_tuples].count('HTTP200')
+
+        print('longest time: {0} lowest time: {1} mean time: {2} median time: {3} deviation: {4} numOf responses: {5} error count: {6} with {7} 20-times crawling users'
+              .format(longest_time, lowest_time, mean_time, median_time, std_deviation, length, error_count, num_of_users))
+        sys.stdout.flush()
 test2()
 
+print("\n\n\n crawl x0 \n\n\n")
 
 # parallel login
 def test3():
-    for num_of_users in [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 200]:
-        number_of_runs = 5
-        total_mean_response_time = 0
-        total_longest_response_time = ('', 0)
-        total_lowest_response_time = ('', 123456789)
+    for num_of_users in [20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300]:
+        number_of_runs = 1
 
+        all_threads = []
         for run in range(number_of_runs):
             clear_cookies()
             thread_count = num_of_users
             cut_length = int(0.1 * thread_count)
-            all_threads = []
 
             for i in range(thread_count):
                 thread = Crawler.Crawler(i, 0)
@@ -142,24 +136,23 @@ def test3():
             for thread in all_threads:
                 thread.join()
 
-            list_of_tuples = [item for sublist in [item.responses for item in all_threads] for item in sublist]
-            if cut_length != 0:
-                list_of_tuples = list_of_tuples[cut_length:-cut_length]
-
-            longest_response_time = max(list_of_tuples, key=lambda item: item[1])
-            if longest_response_time[1] > total_longest_response_time[1]:
-                total_longest_response_time = longest_response_time
-            lowest_response_time = min(list_of_tuples, key=lambda item: item[1])
-            if lowest_response_time[1] < total_lowest_response_time[1]:
-                total_lowest_response_time = lowest_response_time
-            mean_response_time = float(sum(y for x, y in list_of_tuples)) / max(len(list_of_tuples), 1)
-            total_mean_response_time += mean_response_time
-
             sync_utils.running = False
 
-        total_mean_response_time /= number_of_runs
-        print('Longest response time: {0}s, Lowest response time: {1}s Mean response time: {2}s with {3} instant log-in, log-out users'
-              .format(total_longest_response_time, total_lowest_response_time, total_mean_response_time, num_of_users))
+        list_of_tuples = [item for sublist in [item.responses for item in all_threads] for item in sublist]
+        if cut_length != 0:
+            list_of_tuples = list_of_tuples[cut_length:-cut_length]
+
+        longest_time = max(list_of_tuples, key=lambda item: item[1])
+        lowest_time = min(list_of_tuples, key=lambda item: item[1])
+        mean_time = statistics.mean([item[1] for item in list_of_tuples])
+        median_time = statistics.median([item[1] for item in list_of_tuples])
+        std_deviation = statistics.stdev([item[1] for item in list_of_tuples])
+        length = len(list_of_tuples)
+        error_count = length - [item[0] for item in list_of_tuples].count('HTTP200')
+
+        print('longest time: {0} lowest time: {1} mean time: {2} median time: {3} deviation: {4} numOf responses: {5} error count: {6} with {7} instant log-in, log-out users'
+              .format(longest_time, lowest_time, mean_time, median_time, std_deviation, length, error_count, num_of_users))
+        sys.stdout.flush()
 test3()
 
 
